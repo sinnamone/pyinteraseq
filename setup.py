40642dd6025324321a508438bfc0d312e6bdd09d
Simone Puccio <sinnamone@gmail.com>
2017-11-16 17:02:29 +0100

    Comment line added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyinteraseq',
+      version='0.1',
+      description='Interactome-Sequencing package',
+      url='https://github.com/sinnamone/pyinteraseq',
+      author='Simone Puccio',
+      author_email='simone.puccio@itb.cnr.it',
+      license='MIT',
+      packages=['pyinteraseq'],
+      zip_safe=False)
